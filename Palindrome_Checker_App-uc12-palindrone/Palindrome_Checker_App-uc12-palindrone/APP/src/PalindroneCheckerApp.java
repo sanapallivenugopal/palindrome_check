18d11835036e4091b108d9bd1610951f2834f06b
sanapallivenugopal <sanapallivenugopal@gmail.com>
2026-03-11 09:43:55 +0530

    Add files via upload

---
+++ Palindrome_Checker_App-uc12-palindrone/Palindrome_Checker_App-uc12-palindrone/APP/src/PalindroneCheckerApp.java
@@ -0,0 +1,40 @@
+public class PalindroneCheckerApp {
+
+    public static void main(String[] args) {
+
+        String input = "level";
+
+        PalindromeStrategy strategy = new StackStrategy();
+
+        boolean result = strategy.check(input);
+
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + result);
+    }
+}
+
+/* INTERFACE */
+interface PalindromeStrategy {
+    boolean check(String input);
+}
+
+/* STACK STRATEGY */
+class StackStrategy implements PalindromeStrategy {
+
+    public boolean check(String input) {
+
+        java.util.Stack<Character> stack = new java.util.Stack<>();
+
+        for (char c : input.toCharArray()) {
+            stack.push(c);
+        }
+
+        for (char c : input.toCharArray()) {
+            if (c != stack.pop()) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
